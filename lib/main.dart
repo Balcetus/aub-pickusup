a142f8664574392eaa5b273346b0050f65ebea7b
ramihdedeh <115735377+ramihdedeh@users.noreply.github.com>
2023-04-18 16:26:05 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,37 @@
-import 'package:aub_pickusup/pages/signin.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:aub_pickusup/authentication/signup_screen.dart';
+import 'package:aub_pickusup/splashScreen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:page_transition/page_transition.dart';
+import 'mainScreens/Choose.dart';
+import 'mainScreens/main_screenDriver.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/signin',
-      routes: {
-        '/signin': (context) => SignIn(),
-        '/': (context) => const Home(),
-      },
-    ));
+
+
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return   GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage()
+    );
+  }
+}
